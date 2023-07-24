1b51df2a2bd4b6ad9e5d4ebe04710fc5c5de5002
Meonu <yeonugim42@gmail.com>
2023-07-25 02:02:35 +0900

    Solve 1978

---
+++ Factor, Multiple/1978.py
@@ -0,0 +1,20 @@
+def prime_list(n):
+    sieve = [True] * n
+
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우 
+            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
+p = prime_list(1000)
+n = int(input())
+a = list(map(int,input().split()))
+
+sum = 0
+for k in a:
+    if p.count(k):
+        sum += 1
+print(sum)
