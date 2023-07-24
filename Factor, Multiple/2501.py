052b4f9f93fb834c0f5b996e5ec74f95251fbedd
Meonu <yeonugim42@gmail.com>
2023-07-25 01:21:44 +0900

    reinforce 2501

--- Factor, Multiple/2501.py
+++ Factor, Multiple/2501.py
@@ -1,6 +1,7 @@
 a,b = map(int,input().split())
+#this Solve is 52ms
+"""
 k = 0
-
 for i in range(1,a+1):
     if a%i==0:
         if k + 1 == b:
@@ -8,5 +9,10 @@ for i in range(1,a+1):
             exit(0)
         else:
             k+=1
-            
 print(0)
+            """
+#this Solve is 36ms
+try:
+    print([i for i in range(1,a+1) if a%i==0][b-1])
+except:
+    print(0)
