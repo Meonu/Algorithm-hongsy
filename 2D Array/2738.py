640f4539cf302dc2573b280547d1c60b593133fd
Meonu <yeonugim42@gmail.com>
2023-07-24 17:12:22 +0900

    edit

--- 2D Array/2738.py
+++ 2D Array/2738.py
@@ -6,7 +6,7 @@ for i in range(int(n)):
     a.append(list(map(int, input().split())))
 
 for i in range(int(n)):
-    b.append(list(map(int, input().split())))
+    b.append(list(map(int, input().split())))                                                                                                                                                                                                                                           
 
 for i in range(0, int(n)):
     for j in range(0, int(m)):
